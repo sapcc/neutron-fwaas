c1df63a20e5e24665fbfed237183e70bd1e3e549
D072002 <sebastian.wagner02@sap.com>
2021-07-23 13:47:15 +0200

    Allow policies to be bound on gateway interfaces
    The gateway interface is the external side of the router. As we want to
    be able to identify traffic coming from that interface, we also need to be
    able to bind policies on it.

--- neutron_fwaas/services/firewall/fwaas_plugin_v2.py
+++ neutron_fwaas/services/firewall/fwaas_plugin_v2.py
@@ -25,6 +25,7 @@ from neutron_lib.callbacks import resources
 from neutron_lib import constants as nl_constants
 from neutron_lib.db import api as db_api
 from neutron_lib.exceptions import firewall_v2 as f_exc
+from neutron_lib.plugins import constants as plugin_const
 from neutron_lib.plugins import directory
 from oslo_log import helpers as log_helpers
 from oslo_log import log as logging
@@ -67,6 +68,7 @@ class FirewallPluginV2(Firewallv2PluginBase):
 
         self.driver_name = default_provider
         self.driver = drivers[default_provider]
+        self.l3_plugin = directory.get_plugin(plugin_const.L3)
 
         # start rpc listener if driver required
         if isinstance(self.driver, driver_api.FirewallDriverRPCMixin):
@@ -154,7 +156,17 @@ class FirewallPluginV2(Firewallv2PluginBase):
         for port_id in fwg_ports:
             port = self._core_plugin.get_port(context, port_id)
             device_owner = port.get('device_owner', '')
-            if device_owner in nl_constants.ROUTER_INTERFACE_OWNERS:
+
+            if device_owner == nl_constants.DEVICE_OWNER_ROUTER_GW:
+                # The gateway interface has no tenant, test if the gateway interface's router
+                # is owned by the tenant requestor
+                router = self.l3_plugin.get_router(context, port['device_id'])
+                if router:
+                    port_tenant_id = router['tenant_id']
+                    if port_tenant_id != tenant_id:
+                        raise f_exc.FirewallGroupPortInvalidProject(
+                            port_id=port_id, project_id=port['tenant_id'])
+            elif device_owner in nl_constants.ROUTER_INTERFACE_OWNERS:
                 if not self.driver.is_supported_l3_port(port):
                     raise exceptions.FirewallGroupPortNotSupported(
                         driver_name=self.driver_name, port_id=port_id)
